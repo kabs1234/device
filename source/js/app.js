1726f91bcbc71da22e0c246a94c1bb8d05907f62
Aikhan Zhashkeyev <spy.04@mail.ru>
2024-08-06 19:41:13 +0500

    refactor css

--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,5 @@ const dropdownButton = document.querySelector('.dropdown__button');
 
 dropdownButton.addEventListener('click', () => {
   dropdownMenu.classList.toggle('dropdown__menu--active');
+  dropdownButton.classList.toggle('dropdown__button--active');
 })
